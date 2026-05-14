551855b087b6b514532d6ec5d0c436da3cc060b1
sangwooshin <jsw7460@gmail.com>
2026-05-14 11:01:17 -0500

    Remove unused configs/models_zoo

--- JaxRLWorld/rlworld/rl/configs/models_zoo/go2/go2_models.py
+++
@@ -1,4 +0,0 @@
-physics_encoder_1 = {
-    "path": "./outputs/dynamics_models/physics_hybrid_20251122_161020.pt",
-    "memo": "HybridDynamicsKinematicEncoder를 go2 expert dataset에서 dynamics learning 해둔 encoder pretrained model",
-}
